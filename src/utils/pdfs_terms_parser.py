ea3d767e7263c4032fcc0af8cfb09c731a823a8b
Rocio Tarda <rtarda@fi.uba.ar>
2024-08-28 18:33:02 -0300

    Add order to db files

--- src/utils/pdfs_terms_parser.py
+++ src/utils/pdfs_terms_parser.py
@@ -25,6 +25,18 @@ def upload_data(pdf_directory, thesaurus, database):
     file_db = File(database)
     keyword_db = Keyword(database)
 
+    root_term = thesaurus.get_by_id("1")
+    root_term_children = root_term.get_children()
+
+    root_term_grandchildren = []
+    print("root_term_children",root_term_children)
+
+    for children_id in root_term_children:
+        children_of_children = thesaurus.get_by_id(children_id).get_children()
+        for grandchild_id in children_of_children:
+            root_term_grandchildren.append(grandchild_id)
+        print("root_term_grandchildren",root_term_grandchildren)
+
     file_count = count_files(pdf_directory)
     log.info(f"Saving in db with {file_count} files.")
 
@@ -50,7 +62,11 @@ def upload_data(pdf_directory, thesaurus, database):
 
             if (result != False):
                 for keyword in keywords:
-                    keyword_db.add(file_id=file_id, keyword_id=keyword, order=1)
+                    if keyword in root_term_children or keyword in root_term_grandchildren:
+                        print("Order 1:", keyword)
+                        keyword_db.add(file_id=file_id, keyword_id=keyword, order=1)
+                    else:
+                        keyword_db.add(file_id=file_id, keyword_id=keyword, order=2)
 
             pdf_document.close()
             count += 1
