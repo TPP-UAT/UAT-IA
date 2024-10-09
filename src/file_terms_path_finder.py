d973849632b78e27a139e6d3ad59be22fe76b01c
Rocio Tarda <rtarda@fi.uba.ar>
2024-10-09 18:57:29 -0300

    Improve prints

--- src/file_terms_path_finder.py
+++ src/file_terms_path_finder.py
@@ -8,27 +8,32 @@ def calculate_distances(predicted_ids, original_ids):
     
     distances = {}
 
-    for original_id in original_ids:
-        for predicted_id in predicted_ids:
-            # Find the shortest path between original and predicted
-            shortest_path = thesaurus.find_shortest_path(original_id, predicted_id)
+    for predicted_id in predicted_ids:
+        for original_id in original_ids:
+            # Find the shortest path between predicted and original
+            shortest_path = thesaurus.find_shortest_path(predicted_id, original_id)
 
-            if shortest_path and "1" not in shortest_path:
-                # Save the distance and path if no '1' is present
+            if shortest_path:
                 distances[(predicted_id, original_id)] = (len(shortest_path) - 1, shortest_path)
             else:
-                # If no path is found or path contains '1', return None
-                distances[(predicted_id, original_id)] = (0, "No path")
+                # If no path is found, return None for that pair
+                distances[(predicted_id, original_id)] = (None, "No path found")
     
     return distances
 
 if __name__ == '__main__':
     gc.set_debug(gc.DEBUG_SAVEALL)
-    
     predicted_ids = ["1145", "804", "486"]
     original_ids = ["343", "1383", "1338", "1345", "799"]
 
     result = calculate_distances(predicted_ids, original_ids)
     
+    counter = 0
     for key, value in result.items():
-        print(f"Predicted ID {key[0]}, Original ID {key[1]}: Distance {value[0]}, Path {value[1]}")
+        if counter % len(original_ids) == 0 and counter != 0:
+            print("---------------------")
+        if isinstance(value[1], list) and "1" in value[1]:
+            print(f"Predicted ID {key[0]}, Original ID {key[1]}: No path found")
+        else:
+            print(f"Predicted ID {key[0]}, Original ID {key[1]}: Distance {value[0]}, Path {value[1]}")
+        counter += 1
