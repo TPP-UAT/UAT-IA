2140014f6190fd09d4e0b36ca48609389410e626
Rocio Tarda <rtarda@fi.uba.ar>
2024-10-09 18:28:02 -0300

    Improve to discard paths containing 1

--- src/file_terms_path_finder.py
+++ src/file_terms_path_finder.py
@@ -8,23 +8,25 @@ def calculate_distances(predicted_ids, original_ids):
     
     distances = {}
 
-    for predicted_id in predicted_ids:
-        for original_id in original_ids:
-            # Find the shortest path between predicted and original
-            shortest_path = thesaurus.find_shortest_path(predicted_id, original_id)
+    for original_id in original_ids:
+        for predicted_id in predicted_ids:
+            # Find the shortest path between original and predicted
+            shortest_path = thesaurus.find_shortest_path(original_id, predicted_id)
 
-            if shortest_path:
+            if shortest_path and "1" not in shortest_path:
+                # Save the distance and path if no '1' is present
                 distances[(predicted_id, original_id)] = (len(shortest_path) - 1, shortest_path)
             else:
-                # If no path is found, return None for that pair
-                distances[(predicted_id, original_id)] = (None, "No path found")
+                # If no path is found or path contains '1', return None
+                distances[(predicted_id, original_id)] = (0, "No path")
     
     return distances
 
 if __name__ == '__main__':
     gc.set_debug(gc.DEBUG_SAVEALL)
-    predicted_ids = ["804", "101", "1010"]
-    original_ids = ["1963", "1964", "679"]
+    
+    predicted_ids = ["1145", "804", "486"]
+    original_ids = ["343", "1383", "1338", "1345", "799"]
 
     result = calculate_distances(predicted_ids, original_ids)
     
